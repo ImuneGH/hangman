41ff600caf228072a85a8d86bc7231eb7efa9719
ImuneGH <MartinSotnar93@gmail.com>
2025-12-08 19:31:24 +0100

    feat: přidány komponenty a button do formu v Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
 import "../css/pages/home.css";
+import TextInput from "../components/TextInput";
+import ThemeSelect from "../components/ThemeSelect";
+import DifficultySelect from "../components/DifficultySelect";
 
 const Home = () => {
-  return <form className="home-page-form"></form>;
+  return (
+    <form className="home-page-form">
+      <TextInput />
+      <ThemeSelect />
+      <DifficultySelect />
+      <button>Hrát</button>
+    </form>
+  );
 };
 
 export default Home;
