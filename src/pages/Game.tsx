fafa5f7976110831df132d2cce4f1ee74cff7eef
ImuneGH <MartinSotnar93@gmail.com>
2025-12-16 20:52:19 +0100

    feat: částečné nastylování Game.tsx

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,18 +1,26 @@
 import "../css/pages/game.css";
-// import type { OutletContextType } from "../types/types";
-// import { useEffect } from "react";
-// import { useOutletContext } from "react-router-dom";
-
-// const { label, setLabel } = useOutletContext<OutletContextType>();
-// useEffect(() => {
-//   setLabel("Přezdívka");
-// }, []);
+import GameInfo from "../components/GameInfo";
+import Solution from "../components/Solution";
+import GuessLetters from "../components/GuessLetters";
+import TextInput from "../components/TextInput";
+import type { OutletContextType } from "../types/types";
+import { useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
 
 const Game = () => {
+  const { label, setLabel } = useOutletContext<OutletContextType>();
+  useEffect(() => {
+    setLabel("Hádej celé slovo");
+  }, []);
+
   return (
-    <>
+    <div className="game-layout">
       <h1>basdjals</h1>
-    </>
+      <GameInfo />
+      <Solution />
+      <GuessLetters />
+      <TextInput label={label} />
+    </div>
   );
 };
 
