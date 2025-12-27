d801feb8518726be8e53068ff1b2ad90db3214f7
ImuneGH <MartinSotnar93@gmail.com>
2025-12-27 14:55:02 +0100

    feat: komponenta guessLetters přidána

--- src/components/GuessLetters.tsx
+++ src/components/GuessLetters.tsx
@@ -1,7 +1,12 @@
 import "../css/components/guessLetters.css";
 
 const GuessLetters = () => {
-  return <></>;
+  return (
+    <div className="guess-letters">
+      <h3>Hádej písmena</h3>
+      <div className="letters">A B C D E F G H I J K L M N O P Q R S T U V W X Y Z</div>
+    </div>
+  );
 };
 
 export default GuessLetters;
