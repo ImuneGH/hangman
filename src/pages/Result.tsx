acd81dda8e636fc8ae6007dd447e59fd6e0b5136
ImuneGH <MartinSotnar93@gmail.com>
2025-12-28 14:03:51 +0100

    feat: result nastylován + smazání marginu z defaultu pro p a span

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -2,9 +2,13 @@ import "../css/pages/result.css";
 
 const Result = () => {
   return (
-    <>
-      <h2>Result</h2>
-    </>
+    <div className="result">
+      <span className="final-message">Gratuluji "nick"! Vyhrál(a) jsi!</span>
+      <span className="attempts">Počet pokusů: X</span>
+      <span className="faults">Počet chyb: Y</span>
+      <button className="play-again-btn">Hraj znovu</button>
+      <button className="home-btn">Vrať se na hlavní stranu</button>
+    </div>
   );
 };
 
