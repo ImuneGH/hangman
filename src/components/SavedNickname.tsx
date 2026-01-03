4ba63b49123b97bb09fa97d1a4e5ba50f3c01f79
ImuneGH <MartinSotnar93@gmail.com>
2026-01-03 21:35:33 +0100

    feat: tvorba stavu uloženého nicku v localStorage

--- src/components/SavedNickname.tsx
+++ src/components/SavedNickname.tsx
@@ -3,11 +3,10 @@ import "../css/components/savedNickname.css";
 import type { OutletContextType } from "../types/types";
 
 const SavedNickname = () => {
-  const { nickname } = useOutletContext<OutletContextType>();
-  console.log(nickname);
+  const { savedNickname } = useOutletContext<OutletContextType>();
   return (
     <div className="saved-nickname">
-      <span>Ahoj {nickname}, pojď si zahrát oběšence! Stačí vybrat téma, obtížnost a zmáčknout play!</span>
+      <span>Ahoj {savedNickname}, pojď si zahrát oběšence! Stačí vybrat téma, obtížnost a zmáčknout play!</span>
       <button className="change-nickname">Změň přezdívku</button>
     </div>
   );
