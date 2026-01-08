1903992e67b4570715c35ee2889b9e43ba8998f7
ImuneGH <MartinSotnar93@gmail.com>
2026-01-08 18:49:05 +0100

    feat: přidán fetch jsonu slov ke hře

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
 import "./css/app.css";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+type GameCategory = {
+  nazev: string;
+  slova: string[];
+};
+
+type GameWords = {
+  jidlo: GameCategory;
+  povolani: GameCategory;
+  programovani: GameCategory;
+  sport: GameCategory;
+  zemepis: GameCategory;
+  zvirata: GameCategory;
+};
 
 function App() {
+  const [gameWords, setGameWords] = useState<GameWords | null>(null);
+
+  const fetchGameWords = async () => {
+    try {
+      const response = await fetch("../words.json");
+      if (!response.ok) {
+        throw new Error("Chyba fetche");
+      }
+      const words = await response.json();
+
+      setGameWords(words);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error("Chyba při získávání dat: ", err.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchGameWords();
+  }, []);
+
+  setTimeout(() => {
+    console.log(gameWords);
+  }, 500);
+
   const [savedNickname, setSavedNickname] = useState<string | null>(localStorage.getItem("nickname"));
   const [nickname, setNickname] = useState<string>("");
   return (
