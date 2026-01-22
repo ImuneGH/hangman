91dd33dab5bee81855cca5c42d8940c669340341
ImuneGH <MartinSotnar93@gmail.com>
2026-01-22 18:53:26 +0100

    feat: vytvoření a otypování stavu gameData

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,23 @@ type FormData = {
   difficulty: string | null;
 };
 
+type GameData = {
+  hiddenWord: string;
+  attempts: number;
+  mistakes: number;
+  letters: string[];
+  status: "inGame" | "victory" | "lose";
+};
+
 function App() {
   const [formData, setFormData] = useState<FormData>({ nickname: "", theme: null, difficulty: null });
+  const [gameData, setGameData] = useState<GameData>({
+    hiddenWord: "",
+    attempts: 0,
+    mistakes: 0,
+    letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    status: "inGame",
+  });
   const [gameWords, setGameWords] = useState<GameWords | null>(null);
   const [theme, setTheme] = useState<null | string>(null);
   const [difficulty, setDifficulty] = useState<null | "easy" | "medium" | "hard">(null);
