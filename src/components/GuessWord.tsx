39e9e3947f6bce9742c72721816a8ce7476ab7e0
ImuneGH <MartinSotnar93@gmail.com>
2026-01-25 15:28:44 +0100

    refactor: překopání eventu do react podoby

--- src/components/GuessWord.tsx
+++ src/components/GuessWord.tsx
@@ -1,7 +1,7 @@
 import { useOutletContext } from "react-router-dom";
 import "../css/components/guessWord.css";
 import type { OutletContextType } from "../types/types";
-import { useEffect, useRef, useState, type ChangeEvent } from "react";
+import { useState } from "react";
 
 type GuessWordProps = {
   setGuessedLetters: React.Dispatch<React.SetStateAction<string[]>>;
@@ -10,22 +10,6 @@ type GuessWordProps = {
 const GuessWord = ({ setGuessedLetters }: GuessWordProps) => {
   const [handleWord, setHandleWord] = useState<string>("");
   const { gameData, setGameData } = useOutletContext<OutletContextType>();
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
-  const shortcuts = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      guessWholeWord();
-    }
-  };
-
-  useEffect(() => {
-    inputRef.current && inputRef.current.addEventListener("keyup", shortcuts);
-    return () => {
-      inputRef.current && inputRef.current.removeEventListener("keyup", shortcuts);
-    };
-  }, [handleWord]);
-
-  console.log(gameData.hiddenWord);
 
   const guessWholeWord = () => {
     const formattedWord = handleWord
@@ -45,7 +29,15 @@ const GuessWord = ({ setGuessedLetters }: GuessWordProps) => {
     <div className="guess-word">
       <label htmlFor="guess-word">Hádej celé slovo</label>
       <div className="guess-word-container">
-        <input ref={inputRef} autoComplete="off" type="text" id="guess-word" className="guess-word-input" value={handleWord} onChange={(e) => setHandleWord(e.target.value)} />
+        <input
+          onKeyUp={(e) => e.key === "Enter" && guessWholeWord()}
+          autoComplete="off"
+          type="text"
+          id="guess-word"
+          className="guess-word-input"
+          value={handleWord}
+          onChange={(e) => setHandleWord(e.target.value)}
+        />
         <button onClick={guessWholeWord}>Hádej</button>
       </div>
     </div>
