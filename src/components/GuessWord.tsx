c995b0c2cc648e24513f5c208976c20dfad94596
ImuneGH <MartinSotnar93@gmail.com>
2026-01-25 15:18:23 +0100

    feat: přidání event listeneru na hádání slova

--- src/components/GuessWord.tsx
+++ src/components/GuessWord.tsx
@@ -1,7 +1,7 @@
 import { useOutletContext } from "react-router-dom";
 import "../css/components/guessWord.css";
 import type { OutletContextType } from "../types/types";
-import { useState } from "react";
+import { useEffect, useRef, useState, type ChangeEvent } from "react";
 
 type GuessWordProps = {
   setGuessedLetters: React.Dispatch<React.SetStateAction<string[]>>;
@@ -10,16 +10,34 @@ type GuessWordProps = {
 const GuessWord = ({ setGuessedLetters }: GuessWordProps) => {
   const [handleWord, setHandleWord] = useState<string>("");
   const { gameData, setGameData } = useOutletContext<OutletContextType>();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const shortcuts = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      guessWholeWord();
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current && inputRef.current.addEventListener("keyup", shortcuts);
+    return () => {
+      inputRef.current && inputRef.current.removeEventListener("keyup", shortcuts);
+    };
+  }, [handleWord]);
+
+  console.log(gameData.hiddenWord);
 
   const guessWholeWord = () => {
     const formattedWord = handleWord
       .toLowerCase()
       .normalize("NFD")
       .replace(/[\u0300-\u036f]/g, "");
+    setGameData((prev) => ({ ...prev, attempts: prev.attempts + 1 }));
     if (formattedWord === gameData.hiddenWord) {
-      setGameData((prev) => ({ ...prev, attempts: prev.attempts + 1 }));
       const victoryLetters = formattedWord.split("");
       setGuessedLetters(victoryLetters);
+    } else {
+      setGameData((prev) => ({ ...prev, status: "lose" }));
     }
   };
 
@@ -27,7 +45,7 @@ const GuessWord = ({ setGuessedLetters }: GuessWordProps) => {
     <div className="guess-word">
       <label htmlFor="guess-word">Hádej celé slovo</label>
       <div className="guess-word-container">
-        <input autoComplete="off" type="text" id="guess-word" className="guess-word-input" value={handleWord} onChange={(e) => setHandleWord(e.target.value)} />
+        <input ref={inputRef} autoComplete="off" type="text" id="guess-word" className="guess-word-input" value={handleWord} onChange={(e) => setHandleWord(e.target.value)} />
         <button onClick={guessWholeWord}>Hádej</button>
       </div>
     </div>
