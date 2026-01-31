03ca96332b08aba7c2de39e5a29f85b8fcbfef5f
ImuneGH <MartinSotnar93@gmail.com>
2026-01-31 15:05:35 +0100

    feat: zprovozněny animace na šibenici pomocí useRef

--- src/components/GallowsImg.tsx
+++ src/components/GallowsImg.tsx
@@ -1,6 +1,6 @@
 import { useOutletContext } from "react-router-dom";
 import type { OutletContextType } from "../types/types";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { motion } from "motion/react";
 
 type GallowsImgProps = {
@@ -10,10 +10,20 @@ type GallowsImgProps = {
 const GallowsImg = ({ guessedLetters }: GallowsImgProps) => {
   const { maxAttempts, gameData } = useOutletContext<OutletContextType>();
   const [gallowsProgress, setGallowsProgress] = useState<number>(0);
+  const hasAnimated = useRef({
+    base: false,
+    pole: false,
+    beam: false,
+    rope: false,
+    head: false,
+    body: false,
+    arms: false,
+    legs: false,
+  });
 
   const gallowsAnimation = {
     start: {
-      opacity: 0,
+      opacity: 1,
     },
     end: {
       transition: {
@@ -30,59 +40,190 @@ const GallowsImg = ({ guessedLetters }: GallowsImgProps) => {
   }, [guessedLetters]);
 
   const Base = () => (
-    <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="10" y1="70" x2="40" y2="70" stroke="black" strokeWidth="2" strokeDasharray={30} strokeDashoffset={30} />
+    <motion.line
+      variants={gallowsAnimation}
+      initial="start"
+      animate={!hasAnimated.current.base ? "end" : undefined}
+      onAnimationStart={() => (hasAnimated.current.base = true)}
+      x1="10"
+      y1="70"
+      x2="40"
+      y2="70"
+      stroke="black"
+      strokeWidth="2"
+      strokeDasharray={hasAnimated.current.base ? 0 : 30}
+      strokeDashoffset={hasAnimated.current.base ? 0 : 30}
+    />
   );
   const Pole = () => (
-    <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="25" y1="70" x2="25" y2="10" stroke="black" strokeWidth="2" strokeDasharray={60} strokeDashoffset={60} />
+    <motion.line
+      variants={gallowsAnimation}
+      initial="start"
+      animate={!hasAnimated.current.pole ? "end" : undefined}
+      x1="25"
+      y1="70"
+      x2="25"
+      y2="10"
+      stroke="black"
+      strokeWidth="2"
+      onAnimationStart={() => (hasAnimated.current.pole = true)}
+      strokeDasharray={hasAnimated.current.pole ? 0 : 60}
+      strokeDashoffset={hasAnimated.current.pole ? 0 : 60}
+    />
   );
   const Beam = () => {
     return (
       <>
-        <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="24" y1="10" x2="60" y2="10" stroke="black" strokeWidth="2" strokeDasharray={36} strokeDashoffset={36} />
-        <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="25" y1="25" x2="40" y2="10" stroke="black" strokeWidth="2" strokeDasharray={25} strokeDashoffset={25} />
+        <motion.line
+          variants={gallowsAnimation}
+          initial="start"
+          animate={!hasAnimated.current.beam ? "end" : undefined}
+          x1="24"
+          y1="10"
+          x2="60"
+          y2="10"
+          stroke="black"
+          strokeWidth="2"
+          onAnimationStart={() => (hasAnimated.current.beam = true)}
+          strokeDasharray={hasAnimated.current.beam ? 0 : 36}
+          strokeDashoffset={hasAnimated.current.beam ? 0 : 36}
+        />
+        <motion.line
+          variants={gallowsAnimation}
+          initial="start"
+          animate={!hasAnimated.current.beam ? "end" : undefined}
+          x1="25"
+          y1="25"
+          x2="40"
+          y2="10"
+          stroke="black"
+          strokeWidth="2"
+          onAnimationStart={() => (hasAnimated.current.beam = true)}
+          strokeDasharray={hasAnimated.current.beam ? 0 : 25}
+          strokeDashoffset={hasAnimated.current.beam ? 0 : 25}
+        />
       </>
     );
   };
   const Rope = () => (
-    <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="59" y1="10" x2="59" y2="30" stroke="black" strokeWidth="2" strokeDasharray={20} strokeDashoffset={20} />
+    <motion.line
+      variants={gallowsAnimation}
+      initial="start"
+      animate={!hasAnimated.current.rope ? "end" : undefined}
+      x1="59"
+      y1="10"
+      x2="59"
+      y2="30"
+      stroke="black"
+      strokeWidth="2"
+      onAnimationStart={() => (hasAnimated.current.rope = true)}
+      strokeDasharray={hasAnimated.current.rope ? 0 : 20}
+      strokeDashoffset={hasAnimated.current.rope ? 0 : 20}
+    />
   );
   const Head = () => (
     <motion.circle
       variants={gallowsAnimation}
       initial="start"
-      animate="end"
+      animate={!hasAnimated.current.head ? "end" : undefined}
       cx={59}
       cy={35}
       r={5}
       stroke="black"
       strokeWidth={1}
       fill="none"
-      strokeDasharray={31.4}
-      strokeDashoffset={31.4}
+      onAnimationStart={() => (hasAnimated.current.head = true)}
+      strokeDasharray={hasAnimated.current.head ? 0 : 31.4}
+      strokeDashoffset={hasAnimated.current.head ? 0 : 31.4}
     />
   );
   const Body = () => (
-    <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="59" y1="40" x2="59" y2="50" stroke="black" strokeWidth="2" strokeDasharray={10} strokeDashoffset={10} />
+    <motion.line
+      variants={gallowsAnimation}
+      initial="start"
+      animate={!hasAnimated.current.body ? "end" : undefined}
+      x1="59"
+      y1="40"
+      x2="59"
+      y2="50"
+      stroke="black"
+      strokeWidth="2"
+      onAnimationStart={() => (hasAnimated.current.body = true)}
+      strokeDasharray={hasAnimated.current.body ? 0 : 10}
+      strokeDashoffset={hasAnimated.current.body ? 0 : 10}
+    />
   );
   const Arms = () => {
     return (
       <>
-        <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="59" y1="40" x2="53" y2="46" stroke="black" strokeWidth="2" strokeDasharray={10} strokeDashoffset={10} />
-        <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="59" y1="40" x2="65" y2="46" stroke="black" strokeWidth="2" strokeDasharray={10} strokeDashoffset={10} />
+        <motion.line
+          variants={gallowsAnimation}
+          initial="start"
+          animate={!hasAnimated.current.arms ? "end" : undefined}
+          x1="59"
+          y1="40"
+          x2="53"
+          y2="46"
+          stroke="black"
+          strokeWidth="2"
+          onAnimationStart={() => (hasAnimated.current.arms = true)}
+          strokeDasharray={hasAnimated.current.arms ? 0 : 10}
+          strokeDashoffset={hasAnimated.current.arms ? 0 : 10}
+        />
+        <motion.line
+          variants={gallowsAnimation}
+          initial="start"
+          animate={!hasAnimated.current.arms ? "end" : undefined}
+          x1="59"
+          y1="40"
+          x2="65"
+          y2="46"
+          stroke="black"
+          strokeWidth="2"
+          onAnimationStart={() => (hasAnimated.current.arms = true)}
+          strokeDasharray={hasAnimated.current.arms ? 0 : 10}
+          strokeDashoffset={hasAnimated.current.arms ? 0 : 10}
+        />
       </>
     );
   };
   const Legs = () => {
     return (
       <>
-        <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="59" y1="50" x2="55" y2="57" stroke="black" strokeWidth="2" strokeDasharray={10} strokeDashoffset={10} />
-        <motion.line variants={gallowsAnimation} initial="start" animate="end" x1="59" y1="50" x2="63" y2="57" stroke="black" strokeWidth="2" strokeDasharray={10} strokeDashoffset={10} />
+        <motion.line
+          variants={gallowsAnimation}
+          initial="start"
+          animate={!hasAnimated.current.legs ? "end" : undefined}
+          x1="59"
+          y1="50"
+          x2="55"
+          y2="57"
+          stroke="black"
+          strokeWidth="2"
+          onAnimationStart={() => (hasAnimated.current.legs = true)}
+          strokeDasharray={hasAnimated.current.legs ? 0 : 10}
+          strokeDashoffset={hasAnimated.current.legs ? 0 : 10}
+        />
+        <motion.line
+          variants={gallowsAnimation}
+          initial="start"
+          animate={!hasAnimated.current.legs ? "end" : undefined}
+          x1="59"
+          y1="50"
+          x2="63"
+          y2="57"
+          stroke="black"
+          strokeWidth="2"
+          onAnimationStart={() => (hasAnimated.current.legs = true)}
+          strokeDasharray={hasAnimated.current.legs ? 0 : 10}
+          strokeDashoffset={hasAnimated.current.legs ? 0 : 10}
+        />
       </>
     );
   };
 
   return (
-    <svg viewBox="0 0 80 80" width="80" height="80">
+    <motion.svg viewBox="0 0 80 80" width="80" height="80">
       {gallowsProgress > 0 && <Base />}
       {gallowsProgress > 1 && <Pole />}
       {gallowsProgress > 2 && <Beam />}
@@ -91,7 +232,7 @@ const GallowsImg = ({ guessedLetters }: GallowsImgProps) => {
       {gallowsProgress > 5 && <Body />}
       {gallowsProgress > 6 && <Arms />}
       {gallowsProgress > 7 && <Legs />}
-    </svg>
+    </motion.svg>
   );
 };
 
