7e87ac63daba300c66a3d0a927aac26e8556b91d
ImuneGH <MartinSotnar93@gmail.com>
2026-02-01 14:11:18 +0100

    feat: přidán zvuk kreslení šibenice

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -3,7 +3,7 @@ import GameInfo from "../components/GameInfo";
 import Solution from "../components/Solution";
 import GuessLetters from "../components/GuessLetters";
 import GuessWord from "../components/GuessWord";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Navigate, useOutletContext, useNavigate } from "react-router-dom";
 import type { OutletContextType } from "../types/types";
 
@@ -13,6 +13,7 @@ const Game = () => {
   const navigate = useNavigate();
   const [animationCompleted, setAnimationCompleted] = useState<boolean>(false);
   const [gallowsProgress, setGallowsProgress] = useState<number>(0);
+  const pencilSoundRef = useRef<HTMLAudioElement>(null);
 
   if (!gameData.hiddenWord) {
     return <Navigate to="/" replace />;
@@ -52,8 +53,20 @@ const Game = () => {
     }
   }, [gameData.status, animationCompleted]);
 
+  useEffect(() => {
+    const audio: HTMLAudioElement | null = pencilSoundRef.current;
+    console.log(gameData.mistakes);
+    if (!audio || gameData.mistakes === 0) return;
+    if (audio) {
+      audio.currentTime = 0;
+      audio.volume = 0.8;
+      audio.play();
+    }
+  }, [gameData.mistakes]);
+
   return (
     <div className="game-layout">
+      <audio ref={pencilSoundRef} src="./pencil-sound2.wav" />
       <GameInfo gallowsProgress={gallowsProgress} setGallowsProgress={setGallowsProgress} guessedLetters={guessedLetters} setAnimationCompleted={setAnimationCompleted} />
       <Solution formatWord={formatWord} guessedLetters={guessedLetters} />
       <GuessLetters gallowsProgress={gallowsProgress} formatWord={formatWord} setGuessedLetters={setGuessedLetters} guessedLetters={guessedLetters} />
